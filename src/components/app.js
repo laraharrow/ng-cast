745f4f34f8026bfcbbc3986fef7bae2d605ee17e
Hack Reactor Student <student@hackreactor.com>
2017-09-12 11:44:13 -0700

    completes search binding with video player and list

--- src/components/app.js
+++ src/components/app.js
@@ -3,25 +3,26 @@ angular.module('video-player')
 .component('app', {
   templateUrl: 'src/templates/app.html',
 
-  controller: function($scope) {
+  controller: function($scope, youTube) {
 
-    this.video = exampleVideoData[3] || video;
-    this.videos = exampleVideoData; 
+    this.youTube = youTube;
 
-    $scope.$on('child', (event, data) => {
-      this.video = data;
+    this.searchResults = (data) => {
+      this.videos = data;
+      this.video = data[0];
+    };
+
+    youTube.search(this.searchResults);
+
+    $scope.$on('child', (event, video) => {
+      this.video = video;
     });
 
-    $scope.$on('searchquery', (event, data) => {
-      // this.search = data;
-      this.videos = this.ajaxSearchQuery(data);
+    $scope.$on('searchquery', (event, query) => {
+      this.videos = youTube.search(this.searchResults, query);
       this.video = this.videos[0];
     });
 
-    this.ajaxSearchQuery = () => {
-      
-    };
-    
   }
   
 });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,25 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+.service('youTube', function($http) {
+  this.search = function(cb, query = 'acroyoga') {
+    return $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      headers: {
+        'Content-Type': 'application/json'
+      },  
+      params: {
+        'key': window.YOUTUBE_API_KEY,
+        'maxResults': 5,
+        'part': 'snippet',
+        'q': query,
+        'type': 'video',
+        'videoEmbeddable': 'true'
+      }
+    }).then(function successCallback(response) {
+      cb(response.data.items);
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
+  };
 });
